3e8370c5060db1321e6402a3cab2213deb3791f5
midsum-salrux <idempotato@protonmail.com>
2022-07-09 15:17:21 -0400

    Token page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import BotType from "./BotType";
 import Channels from "./Channels";
 import NavBar, { BOT_TYPE, TOKEN, CHANNELS } from "./NavBar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Token from "./Token"
 import Urbit from "@urbit/http-api";
 
-function componentForPage(page) {
+function componentForPage(page, config, configPoke) {
   if (page == BOT_TYPE) {
-    return <BotType />;
+    return <BotType config={config} configPoke={configPoke} />;
   } else if (page == TOKEN) {
-    return <Token />;
+    return <Token config={config} configPoke={configPoke} />;
   } else if (page == CHANNELS) {
-    return <Channels />;
+    return <Channels config={config} configPoke={configPoke} />;
   } else {
     return <p>Something went wrong</p>;
   }
@@ -19,12 +19,30 @@ function componentForPage(page) {
 
 export default function App() {
   const [page, setPage] = useState(BOT_TYPE);
+  const [config, setConfig] = useState({botToken: "", channels: []});
+
+  async function configScry() {
+    const result = await window.urbit.scry({app: "faux", path: "/faux/config"});
+    setConfig(result);
+  }
+
+  function configPoke(newConfig) {
+    window.urbit.poke({
+      app: "faux",
+      mark: "json",
+      json: newConfig,
+      onSuccess: () => configScry()
+    });
+  }
+
+  useEffect(() => configScry(), []);
+
 
   window.urbit = new Urbit("");
   window.urbit.ship = window.ship;
 
   return <>
     <NavBar page={page} setPage={setPage} />
-    {componentForPage(page)}
+    {componentForPage(page, config, configPoke)}
   </>;
 }
--- frontend/src/Token.jsx
+++ frontend/src/Token.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Token() {
-  return <>
-    <p>Configure your token</p>
-  </>
+export default function Token(props) {
+  const [botToken, setBotToken] = useState("");
+
+  function pokeBotToken() {
+    props.configPoke({botToken: botToken, channels: props.config.channels});
+  }
+
+  return <div className="container">
+    <div className="row">
+      <div className="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 className="display-4">Token</h1>
+        {props.config.botToken ? <p className="lead">Your current token is <code>{props.config.botToken}</code></p> : ""}
+        <div className="input-group">
+          <input type="text" id="botToken" className="form-control" placeholder="New Token"
+                 onChange={(e) => setBotToken(e.target.value)} />
+          <div className="input-group-append">
+            <button className="btn btn-primary" onClick={pokeBotToken}>Continue</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 }
