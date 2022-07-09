ffa30ade16c4a1155cd2bec330ab6659c2227d66
midsum-salrux <idempotato@protonmail.com>
2022-07-09 11:28:19 -0400

    Bootstrap & icon buttons

---
+++ frontend/src/Config.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+
+export default function Config() {
+  const [config, setConfig] = useState({botToken: "", channels: []});
+  const [botToken, setBotToken] = useState("");
+  const [resource, setResource] = useState("");
+  const [discordChannel, setDiscordChannel] = useState("");
+  const [latestMessage, setLatestMessage] = useState("");
+
+  async function configScry() {
+    const result = await window.urbit.scry({app: "faux", path: "/faux/config"});
+      setConfig(result);
+  }
+
+  function configPoke(newConfig) {
+    window.urbit.poke({
+      app: "faux",
+      mark: "json",
+      json: newConfig,
+      onSuccess: () => configScry()
+    });
+  }
+
+  function addChannel() {
+    const [ship, name] =  resource.replace("~", "").split("/");
+    const newChannels = config.channels
+
+    newChannels.push({
+      discordChannelId: discordChannel,
+      lastSeenMessage: latestMessage,
+      resource: {
+        name: name,
+        ship: ship
+      }
+    });
+
+    configPoke({botToken: config.botToken,
+                channels: newChannels});
+  }
+
+  function removeChannel(i) {
+    let newChannels = config.channels;
+    newChannels.splice(i, 1);
+
+    configPoke({botToken: config.botToken, channels: newChannels});
+  }
+
+  function pokeBotToken() {
+    configPoke({botToken: botToken, channels: config.channels});
+  }
+
+  useEffect(() => configScry(), []);
+
+  const channels = config.channels.map((channel, i) =>
+    <li key={i}>
+      {channel.resource.ship + "/" + channel.resource.name} linked to {channel.discordChannelId}
+      <button type="button" className="btn btn-default" onClick={() => removeChannel(i)}>
+        <span className="glyphicon glyphicon-trash"></span>
+      </button>
+    </li>
+  );
+
+  return <>
+    <h2>Bot Token</h2>
+    <p>{config.botToken}</p>
+    <input id="botToken" type="text" value={botToken}
+           onChange={(e) => setBotToken(e.target.value)}
+           placeholder="New Bot Token" />
+    <button className="btn btn-default" onClick={pokeBotToken}>
+      <span className="glyphicon glyphicon-ok"></span>
+    </button>
+    <h2>Channels</h2>
+    <ul>{channels}</ul>
+    <h3>New Channel</h3>
+    <input id="resource" type="text" value={resource}
+           onChange={(e) => setResource(e.target.value)}
+           placeholder="Urbit group resource" />
+    <input id="discordChannel" type="text" value={discordChannel}
+           onChange={(e) => setDiscordChannel(e.target.value)}
+           placeholder="Channel ID" />
+    <input id="latestMessage" type="text" value={latestMessage}
+           onChange={(e) => setLatestMessage(e.target.value)}
+           placeholder="Latest message ID" />
+    <button className="btn btn-default" onClick={addChannel}>
+      <span className="glyphicon glyphicon-plus"></span>
+    </button>
+  </>;
+}
